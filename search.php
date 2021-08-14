c8293b2a5666ccc630c3fd8464753af9010d7fc9
shaykat18-36909-1 <85491225+shaykat18-36909-1@users.noreply.github.com>
2021-08-14 23:42:10 +0600

    Add files via upload

---
+++ search.php
@@ -0,0 +1,23 @@
+<head><title>search</title>  </head>
+<body>
+<fieldset>
+ <font color="red"><h2><b>Page 2 [search ]</b></h1></font>
+
+  <a  target="_blank" href="welcome.php">1.PATIENT HOME</a>
+ 
+ <a  target="_blank" href="search-appoinment officer.php">3.History</a>
+<a  target="_blank" href="search-patient.php">3.FIND DOCTOR</a>
+<a  target="_blank" href="search-admin.php">3.FIND DONOR</a>
+ <br>
+ 
+ 
+  <font color="red"><h2>search donor</h1></font>
+ <select>
+ <option value="0" style ="display:none" selected> SEARCH</option>
+ <option>admin</option>
+ <option>patient</option>
+ <option>appoinment officer</option>
+ </select><br><br>
+ 
+ </fieldset>
+ </body>
